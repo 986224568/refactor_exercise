97aaa7db16a68184de6a4ae13d6fdba1f075c415
Domingo <986224568@qq.com>
2020-08-27 17:19:34 +0800

    [refactor_exercise][Domingo] feature print in html

--- src/statement.js
+++ src/statement.js
@@ -12,8 +12,38 @@ function statement (invoice, plays) {
   return result;
 }
 
+
+function statementHtml (invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = initResultHtml(invoice);
+  const format = forMat();
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+    ({ thisAmount, volumeCredits, result, totalAmount } = getInfoFromPerformanceHtml(play, thisAmount, perf, volumeCredits, result, format, totalAmount));
+  }
+  result = addAmountCreditsHtml(result, format, totalAmount, volumeCredits);
+  return result;
+}
+
+// function renderHtml (customer, performances, totalAmount, volumeCredits) {
+//   let result = `<h1>Statement for ${customer}</h1>\n`;
+//   result += '<table>\n';
+//   result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>';
+//   for (let pref of performances) {
+//     result += ` <tr><rd>${pref.play.name}</td><td>${pref.audience}</td>`;
+//     result += `<td>${usd(pref.amount)}</td></tr>\n`;
+//   }
+//   result += '</table>\n';
+//   result += `<p>Amount owed is <em>${usd(totalAmount)}</em></p>\n`;
+//   result += `<p>You earned <em>${volumeCredits}</em> credits</p>\n`;
+//   return result;
+// }
+
 module.exports = {
   statement,
+  statementHtml,
 };
 
 
@@ -21,9 +51,23 @@ function initResult(invoice) {
   return `Statement for ${invoice.customer}\n`;
 }
 
+function initResultHtml(invoice) {
+  let result = `<h1>Statement for ${invoice.customer}</h1>\n`;
+  result += '<table>\n';
+  result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>';
+  return result;
+}
+
 function addAmountCredits(result, format, totalAmount, volumeCredits) {
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits \n`;
+  return result;
+}
+
+function addAmountCreditsHtml(result, format, totalAmount, volumeCredits) {
+  result += '</table>\n';
+  result += `<p>Amount owed is <em>${format(totalAmount / 100)}</em></p>\n`;
+  result += `<p>You earned <em>${volumeCredits}</em> credits</p>\n`;
   return result;
 }
 
@@ -44,6 +88,22 @@ function getInfoFromPerformance(play, thisAmount, perf, volumeCredits, result, f
     volumeCredits += Math.floor(perf.audience / 5);
   //print line for this order
   result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+  totalAmount += thisAmount;
+  return { thisAmount, volumeCredits, result, totalAmount };
+}
+
+function getInfoFromPerformanceHtml(play, thisAmount, perf, volumeCredits, result, format, totalAmount) {
+  thisAmount = getTimeOut(play, thisAmount, perf);
+  // add volume credits
+  volumeCredits += Math.max(perf.audience - 30, 0);
+  // add extra credit for every ten comedy attendees
+  if ('comedy' === play.type)
+    volumeCredits += Math.floor(perf.audience / 5);
+  //print line for this order
+
+  result += ` <tr><td>${play.name}</td><td>${perf.audience}</td>`;
+  result += `<td>${format(thisAmount / 100)}</td></tr>\n`;
+
   totalAmount += thisAmount;
   return { thisAmount, volumeCredits, result, totalAmount };
 }
